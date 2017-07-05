87cbcb6de7f4b4556a5cbfcf926753946edbbc50
ChrisDrobnicky <krizzztofer@yahoo.com>
2017-07-05 20:20:40 +0200

    add styles to list

--- src/components/ToDoList/ToDoList.component.js
+++ src/components/ToDoList/ToDoList.component.js
@@ -18,20 +18,20 @@ const ToDoList = (props) => {
         {props.listOfTasks.map((item, index) => <div className="item" key={index}>
             <div className={`${styles.toDoListItem} ${styles[item.isDone ? 'greenBoxShadow' : 'redBoxShadow']}`}>
               <span className={styles.itemIndex}>{index + 1}.</span>
-              {item.title}
-              <span className={styles.priorityWrapper}>
-                <i
-                  className = {`circle
-                  ${item.priority === PRIORITY_LOW ? 'yellow'
-                    : item.priority === PRIORITY_NORMAL ? 'orange' : 'red'} icon`}
-                ></i>
-              </span>
+              <span className={styles.itemName}>{item.title}</span>
               <span className={styles.iconWrapper}>
                 <i
                   className={`thumbs outline ${item.isDone ? 'up' : 'down'} icon`}
                   onClick={() => handleDoneOnClick(item.id)}
                 ></i>
                 <i className="trash outline icon" onClick={() => handleDeleteOnClick(item.id)}></i>
+              </span>
+              <span className={styles.priorityWrapper}>
+                <i
+                  className = {`circle
+                  ${item.priority === PRIORITY_LOW ? 'yellow'
+                    : item.priority === PRIORITY_NORMAL ? 'orange' : 'red'} icon`}
+                ></i>
               </span>
             </div>
           </div>
