51b2ceb6b52b3936ec6db7f5a34cdabd785c1073
ChrisDrobnicky <krizzztofer@yahoo.com>
2017-07-09 20:05:52 +0200

    tooltips in progress

--- src/components/AddToDo/AddToDo.component.js
+++ src/components/AddToDo/AddToDo.component.js
@@ -6,6 +6,8 @@ class AddToDo extends Component {
     super();
     this.handleAddTodoClick = this.handleAddTodoClick.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormClick = this.handleFormClick.bind(this);
+    this.handleDescriptionClick = this.handleDescriptionClick.bind(this);
     this.state = {
       taskName: ''
     }
@@ -21,15 +23,31 @@ class AddToDo extends Component {
     this.setState({ taskName: event.target.value });
   }
 
+  handleFormClick(event) {
+    event.preventDefault();
+  }
+
+  handleDescriptionClick() {
+   alert('i need class');
+  }
+
   render() {
     return (
       <section className={styles.AddToDo}>
-        <div className="ui huge action input">
-          <input type="text" placeholder="Add ToDo..." onChange={this.handleInputChange} value={this.state.taskName} />
-          <button className="ui icon positive button" onClick={this.handleAddTodoClick}>
-            <i className="plus large icon"></i>
-          </button>
-        </div>
+        <form className="ui form" onClick={this.handleFormClick}>
+          <div className="ui huge action input">
+            <input type="text" placeholder="Add ToDo..." onChange={this.handleInputChange} value={this.state.taskName} />
+            <button className="ui icon positive button" id="description" onClick={this.handleAddTodoClick}>
+              <i className="plus large icon"></i>
+            </button>
+          </div>
+          <div className="field">
+            <button className={`ui teal icon button ${styles.button}`} onClick={this.handleDescriptionClick}>
+              <i className="arrow circle outline down big icon"></i>
+            </button>
+            <textarea rows="2" className={styles.descriptionDisabled}></textarea>
+          </div>
+        </form>
       </section>
     )
   }
